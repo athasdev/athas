0efc2c36f48afe9ee35026c6dfd795c7dc333745
Finesssee <90105158+Finesssee@users.noreply.github.com>
2026-05-09 22:48:45 +0700

    Match CI Rust formatting

--- crates/tooling/src/types.rs
+++ crates/tooling/src/types.rs
@@ -137,7 +137,8 @@ impl fmt::Display for ToolError {
    }
 }
 
-impl std::error::Error for ToolError {}
+impl std::error::Error for ToolError {
+}
 
 impl From<std::io::Error> for ToolError {
    fn from(err: std::io::Error) -> Self {
